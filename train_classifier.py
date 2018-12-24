dea3af94d067291ab0478a3f24fd3780d729dacc
David <d.d.hamilton@outlook.com>
2018-12-24 14:12:23 +0000

    Defined new project file structure

--- train_classifier.py
+++
@@ -1,9 +0,0 @@
-'''
-A Python script which contains a machine learning pipeline that:
-1. Loads data from the SQLite database
-2. Splits the dataset into training and test sets
-3. Builds a text processing and machine learning pipeline
-4. Trains and tunes a model using GridSearchCV
-5. Outputs results on the test set
-6. Exports the final model as a pickle file
-'''
